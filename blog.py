7d270832161d0b07aa0f1c0f6d1e02b6005dcca8
lucas2nervous <118286517+lucas2nervous@users.noreply.github.com>
2025-06-05 20:12:15 +0800

    Add files via upload

---
+++ blog.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+import datetime
+
+url = "https://pixelford.com/blog/"
+response = requests.get(url, headers = {"user-agent": 'Hello'})
+html = response.content
+soup = BeautifulSoup(html, "html.parser")
+blogs = soup.find_all("article")
+
+for blog in blogs:
+    title = blog.find("a", class_= "entry-title-link").get_text()
+
+    blog_datetime_string = blog.find("time", class_ = "entry-time").get("datetime")
+    blog_datetime = datetime.datetime.fromisoformat(blog_datetime_string)
+    pretty_date = blog_datetime.strftime("%A %B %d, %Y %m %h")
+    print(f"{pretty_date} - {title}")
